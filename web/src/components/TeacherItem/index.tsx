9e748c1a7cd3e9d43a2aa8be246bc89ac1aa1cc4
Tháygoro <thaygoro@gmail.com>
2020-08-09 20:12:04 -0300

    Added button to teacherlist page; Connect to api.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,52 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/23264147?s=460&u=4e983c8b5d679ece1edb4b325f8fccfe05044412&v=4" alt="Tháygoro Minuzzi"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Tháygoro Minuzzi</strong>
-          <span>Programação</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui minima sit cupiditate vitae cumque iusto harum facilis? Facilis doloribus fuga, harum eveniet ut nam, praesentium eius repellendus neque sunt magnam?
-        <br /><br />
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis ea consectetur officia, error odit hic! Dolores fugit, eligendi consequatur quisquam reprehenderit, commodi vel error officia rem ipsam voluptas voluptatibus exercitationem.
+        {teacher.bio}  
       </p>
+
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 100,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
